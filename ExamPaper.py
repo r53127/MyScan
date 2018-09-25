56c3e75bf300efb2cbe9934dc63e93f7344b27df
r53127 <rnkn@163.com>
2018-09-25 09:45:21 +0800

    continue exampaper function

--- ExamPaper.py
+++ ExamPaper.py
@@ -1,80 +1,145 @@
 import cv2 as cv
 import numpy as np
-from imutils import contours, auto_canny
+from imutils import contours
 from imutils.perspective import four_point_transform
 
-ANSWER_CHAR = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E"}
+ANSWER_CHAR = {0: "A", 1: "B", 2: "C", 3: "D"}
 
 
 class ExamPaper():
     def __init__(self):
         self.showingImg = None
 
-    def initProcess(self, imgFile):
-        self.img = cv.imread(imgFile)
-        self.showingImg = self.img
-        self.gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)  # 转化成灰度图片
-        self.gaussian_bulr = cv.GaussianBlur(self.gray, (5, 5), 0)  # 高斯模糊
-        self.edged = cv.Canny(self.gaussian_bulr, 75, 200)  # 边缘检测,灰度值小于2参这个值的会被丢弃，大于3参这个值会被当成边缘，在中间的部分，自动检测
+    def cv_imread(self, file_path=""):
+        img = cv.imdecode(np.fromfile(file_path, dtype=np.uint8), -1)  # 解决不能读取中文路径问题
+        return img
 
-    def test(self,imgFile):
-        img = cv.imread(imgFile)
-        img=self.get_init_process_img(img)
-        cv.imshow('img',img)
+    def initProcess(self, imgFile):
+        self.img = self.cv_imread(imgFile)
+        cv.imshow('1.origin', self.img)
         cv.waitKey(0)
 
+        self.gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)  # 转化成灰度图片
+        cv.imshow('2.1 gray', self.gray)
+        cv.waitKey(0)
 
-    def getMaxContour(self):
-        print('获取最大轮廓')
-        image, cnts, hierarchy = cv.findContours(self.edged.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        ret, thresh2 = cv.threshold(self.gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+        cv.imshow('thresh2', thresh2)
+        cv.waitKey(0)
+
+        image, cnts, hierarchy = cv.findContours(thresh2.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # print("1找到轮廓个数：", len(cnts))
+        # print(cnts)
+        # a=self.img.copy()
+        # cv.drawContours(a, cnts, -1, (255, 0, 0), 2)
+        # cv.imshow('temp', a)
+        # cv.waitKey(0)
+        # print(len(cnts),len(hierarchy),hierarchy[0][0],len(hierarchy[0][0]))
+
+        return cnts
+
+    def get_max_img(self, cnts, src_img):
+        # 按面积大小对所有的轮廓排序
         maxcnt = max(cnts, key=lambda c: cv.contourArea(c))
-        return maxcnt
-
-    def getChoiceContour(self, cnt):
-        print('获取答题区域')
-        peri = 0.01 * cv.arcLength(cnt, True)
+        peri = 0.1 * cv.arcLength(maxcnt, True)
         # 获取多边形的所有定点，如果是四个定点，就代表是矩形
-        approx = cv.approxPolyDP(cnt, peri, True)
+        approx = cv.approxPolyDP(maxcnt, peri, True)
         if len(approx) == 4:  # 矩形
-            # 透视变换提取灰度图内容部分
-            self.tx_sheet = four_point_transform(self.gray, approx.reshape(4, 2))
-            # 使用ostu二值化算法对灰度图做一个二值化处理
-            self.ret, self.thresh2 = cv.threshold(self.tx_sheet, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
-            # 继续寻找轮廓
-            r_image, r_cnt, r_hierarchy = cv.findContours(self.thresh2.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-            print("找到轮廓个数：", len(r_cnt))
-            choiceCnts = []
-            for cxx in r_cnt:
-                # 通过矩形，标记每一个指定的轮廓
-                x, y, w, h = cv.boundingRect(cxx)
-                ar = w / float(h)
+            # 透视变换提取原图内容部分
+            maxImg = four_point_transform(src_img, approx.reshape(4, 2))
+        return maxImg
 
-                if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
-                    # 使用红色标记，满足指定条件的图形
-                    # cv.rectangle(ox_sheet, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                    # 把每个选项，保存下来
-                    choiceCnts.append(cxx)
+    def get_roi_img(self, src_img):
+        gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)  # 转化成灰度图片
+        ret, thresh2 = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+        r_image, cnts, r_hierarchy = cv.findContours(thresh2.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        roi_img = []
+        # 按面积大小对所有的轮廓排序
+        listcnt = sorted(cnts, key=cv.contourArea, reverse=True)
+        dstImgCount = 4  # 获取最大的4个矩形
+        for cnt in listcnt:
+            if dstImgCount == 0:
+                break
+            peri = 0.01 * cv.arcLength(cnt, True)
+            # 获取多边形的所有定点，如果是四个定点，就代表是矩形
+            approx = cv.approxPolyDP(cnt, peri, True)
+            if len(approx) == 4:  # 矩形
+                # 透视变换提取源图内容部分
+                roi_img.append(four_point_transform(src_img, approx.reshape(4, 2)))
+                dstImgCount = dstImgCount - 1
+        return roi_img
+
+    def test(self, imgFile):
+        # 预处理获取所有轮廓
+        cts = self.initProcess(imgFile)
+        # 获取最大的答题卡区域
+        self.paper_img = self.get_max_img(cts, self.img)
+        cv.imshow('paper_img', self.paper_img)
+        cv.waitKey(0)
+        # 找到答题卡上的答题、班级和学号区域
+        self.answer_img = self.get_roi_img(self.paper_img)
+        for i, c in enumerate(self.answer_img):
+            cv.imshow('answer_img' + str(i), c)
+            cv.waitKey(0)
+        # 读取答题区域的选项
+        self.ans_choices_cnts = self.getChoiceContour(self.answer_img[1])
+        self.cls_choice_cnts  = self.getChoiceContour(self.answer_img[2])
+        self.stuid_choice_cnts= self.getChoiceContour(self.answer_img[3])
+
+        self.ans_choices=self.getChoices(self.ans_choices_cnts,self.answer_img[1])
+        print(self.ans_choices)
+
+
+    def getChoiceContour(self, src_img):
+        print('获取答题区域')
+        gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)  # 转化成灰度图片
+        ret, thresh2 = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+        r_image, cnts, r_hierarchy = cv.findContours(thresh2.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        choiceCnts = []
+        for cxx in cnts:
+            # 通过矩形，标记每一个指定的轮廓
+            x, y, w, h = cv.boundingRect(cxx)
+            ar = w / float(h)
+
+            if w < 50 and h < 30 and ar >= 2 and ar <= 3.5 and w > 10 and h > 5:
+                # 使用红色标记，满足指定条件的图形
+                cv.rectangle(src_img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv.imshow('choices', src_img)
+                # 把每个选项，保存下来
+                choiceCnts.append(cxx)
+        print('找到的选项个数为：', len(choiceCnts))
         return choiceCnts
 
-    def getChoices(self, choiceCnts):
+    def getChoices(self, choiceCnts,src_img):
         print('获取所有選項气泡')
+        gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)  # 转化成灰度图片
+        ret, thresh2 = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
         # 按坐标从上到下排序
+        print(len(choiceCnts))
+        choiceCnts = contours.sort_contours(choiceCnts, method="left-to-right")[0]
         choiceCnts = contours.sort_contours(choiceCnts, method="top-to-bottom")[0]
+        print(choiceCnts)
         # 使用np函数，按5个元素，生成一个集合
         choices = []
         # questionID为題号，j为行内序号
-        for (questionID, i) in enumerate(np.arange(0, len(choiceCnts), 5)):
+        for (questionID, i) in enumerate(np.arange(0, len(choiceCnts), 4)):
             # 获取按从左到右的排序后的5个元素
-            cnts = contours.sort_contours(choiceCnts[i:i + 5])[0]
+            cnts = contours.sort_contours(choiceCnts[i:i + 4])[0]
             # 遍历每一个选项
             bubble_row = []  # 暂存每行序号和像素值
             for (inlineID, c) in enumerate(cnts):
                 # 生成一个大小与透视图一样的全黑背景图布
-                mask = np.zeros(self.tx_sheet.shape, dtype="uint8")
+                mask = np.zeros(gray.shape, dtype="uint8")
+                cv.imshow('mask',mask)
+                cv.waitKey(0)
                 # 将指定的轮廓+白色的填充写到画板上,255代表亮度值，亮度=255的时候，颜色是白色，等于0的时候是黑色
                 cv.drawContours(mask, [c], -1, 255, -1)
+                cv.imshow('draw mask',mask)
+                cv.waitKey(0)
                 # 做两个图片做位运算，把每个选项独自显示到画布上，为了统计非0像素值使用，这部分像素最大的其实就是答案
-                mask = cv.bitwise_and(self.thresh2, self.thresh2, mask=mask)
+                mask = cv.bitwise_and(thresh2, thresh2, mask=mask)
+                cv.imshow('bitwise',mask)
+                cv.waitKey(0)
                 # 获取每个答案的像素值
                 total = cv.countNonZero(mask)
                 # 存到一个数组里面，tuple里面的参数分别是，像素大小和行内序号
@@ -85,34 +150,3 @@ class ExamPaper():
             choice_num = bubble_row[0][1]  # [0][0]為total，[0][1]為選項號
             choices.append((questionID + 1, ANSWER_CHAR[choice_num]))  # %为字符串占位操作符
         return choices
-
-    def get_init_process_img(self,roi_img):
-        """
-        对图片进行初始化处理，包括，梯度化，高斯模糊，二值化，腐蚀，膨胀和边缘检测
-        :param roi_img: ndarray
-        :return: ndarray
-        """
-        h = cv.Sobel(roi_img, cv.CV_32F, 0, 1, -1)
-        v = cv.Sobel(roi_img, cv.CV_32F, 1, 0, -1)
-        img = cv.add(h, v)
-        img = cv.convertScaleAbs(img)
-        img = cv.GaussianBlur(img, (3, 3), 0)
-        ret, img = cv.threshold(img, 120, 255, cv.THRESH_BINARY)
-        kernel = np.ones((1, 1), np.uint8)
-        img = cv.erode(img, kernel, iterations=1)
-        img = cv.dilate(img, kernel, iterations=2)
-        img = cv.erode(img, kernel, iterations=1)
-        img = cv.dilate(img, kernel, iterations=2)
-        img = auto_canny(img)
-        return img
-
-    def get_max_area_cnt(self,img):
-        """
-        获得图片里面最大面积的轮廓
-        :param img: ndarray
-        :return: ndarray
-        """
-        image, cnts, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        cnt = max(cnts, key=lambda c: cv.contourArea(c))
-        return cnt
-
--- cvtest.py
+++ cvtest.py
@@ -1,29 +1,67 @@
 import cv2 as cv
 import os
 import numpy as np
+from scipy import ndimage
 
-randomBA=bytearray(os.urandom(120000))
-flatNA=np.array(randomBA)
+# randomBA=bytearray(os.urandom(120000))
+# flatNA=np.array(randomBA)
+#
+# gray=flatNA.reshape(100,300,4)
+# cv.imshow('gray',gray)
+# cv.waitKey(0)
+#
+# bgr=flatNA.reshape(100,400,3)
+# cv.imshow('bgr',bgr)
+# cv.waitKey(0)
+#
+#
+# img=cv.imread(r"image\model.bmp")
+# cv.imshow('bgr',img)
+# cv.waitKey(0)
+# print(img.shape,img.size,img.dtype)
+# roi=img[0:100,0:100]
+# img[200:300,300:400]=roi
+# img[:,:,1]=0
+# print(np.array(img))
+# cv.imshow('bgr',img)
+# cv.waitKey(0)
+# 
+# kernel_3x3 = np.array([[-1, -1, -1],
+#                    [-1,  8, -1],
+#                    [-1, -1, -1]])
+# 
+# kernel_5x5 = np.array([[-1, -1, -1, -1, -1],
+#                        [-1,  1,  2,  1, -1],
+#                        [-1,  2,  4,  2, -1],
+#                        [-1,  1,  2,  1, -1],
+#                        [-1, -1, -1, -1, -1]])
+# 
+# img = cv.imread(r"image\model.bmp",0)
+# img1=cv.imread(r"image\model.bmp")
+# k3 = ndimage.convolve(img, kernel_3x3)
+# k5 = ndimage.convolve(img, kernel_5x5)
+# gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)  # 转化成灰度图片
+# gray1 = cv.GaussianBlur(gray, (17,17), 0)
+# blurred = cv.GaussianBlur(img, (17,17), 0)
+# g_hpf = img - blurred
+# 
+# cv.imshow("img", img)
+# cv.imshow("img1", img1)
+# cv.imshow("3x3", k3)
+# cv.imshow("5x5", k5)
+# cv.imshow("gray", gray)
+# cv.imshow("blurred", blurred)
+# cv.imshow("g_hpf", g_hpf)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
-gray=flatNA.reshape(100,300,4)
-cv.imshow('gray',gray)
-cv.waitKey(0)
+img = np.zeros((200, 200), dtype=np.uint8)
+img[50:150, 50:150] = 255
 
-bgr=flatNA.reshape(100,400,3)
-cv.imshow('bgr',bgr)
-cv.waitKey(0)
-
-
-img=cv.imread(r"image\model.bmp")
-cv.imshow('bgr',img)
-cv.waitKey(0)
-print(img.shape,img.size,img.dtype)
-roi=img[0:100,0:100]
-img[200:300,300:400]=roi
-img[:,:,1]=0
-print(np.array(img))
-
-
-
-cv.imshow('bgr',img)
-cv.waitKey(0)
+ret, thresh = cv.threshold(img, 127, 255, 0)
+image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+color = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+img = cv.drawContours(color, contours, -1, (0,255,0), 2)
+cv.imshow("contours", color)
+cv.waitKey()
+cv.destroyAllWindows()

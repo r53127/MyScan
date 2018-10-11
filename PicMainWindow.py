9d726d432739fd1709604902467793bcdad197dc
r53127 <rnkn@163.com>
2018-10-11 10:11:32 +0800

    clean bug

--- PicMainWindow.py
+++ PicMainWindow.py
@@ -51,7 +51,6 @@ class PicMainWindow(QMainWindow, Ui_MainWindow):
         self.dateEdit.setDateTime(QDateTime.currentDateTime())
         # 刷新班级控件
         self.updateComboBox()
-        self.setWindowIcon(QIcon("scan.ico"))
         # 显示窗体
         self.showMaximized()
 
